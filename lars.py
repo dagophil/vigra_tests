39996e607b7549cdfa69462d9b8aaed99877910b
Philip Schill <philip.schill@gmx.de>
2015-10-22 10:24:25 +0200

    The grouped forest garrote is now parallel.

--- lars.py
+++ lars.py
@@ -8,6 +8,7 @@ import numpy
 import argparse
 import warnings
 from sklearn.utils import ConvergenceWarning
+from multiprocessing import Process, Queue, cpu_count
 
 
 parser = argparse.ArgumentParser(description="Forest garrote script")
@@ -17,6 +18,19 @@ parser.add_argument("-n", type=str, required=True,
                     help="name of the algorithm")
 parser.add_argument("-a", type=float, default=0.0003, help="alpha value of forest garrote")
 parser.add_argument("-g", type=int, nargs="*", default=[], help="indices where the group splits are made")
+parser.add_argument("--n_threads", type=int, default=1, help="number of threads")
+
+
+def lars_worker(in_qu, out_qu, m, labels, alpha):
+    while True:
+        item = in_qu.get()
+        if item is None:
+            break
+        i, begin, end = item
+        sub_m = m[:, begin:end]
+        coefs = sklearn.linear_model.lasso_path(sub_m, labels, positive=True, max_iter=100, alphas=[alpha])[1]
+        coefs = coefs[:, -1]
+        out_qu.put((i, coefs))
 
 
 def main(args):
@@ -55,16 +69,41 @@ def main(args):
         # Make groups and run the forest garrote on each group.
         group_splits = [0] + group_splits + [m.shape[1]]
         n_groups = len(group_splits)-1
-        coef_list = []
-        for i in xrange(n_groups):
-            begin = group_splits[i]
-            end = group_splits[i+1]
-            sub_m = m[:, begin:end]
-            coefs = sklearn.linear_model.lasso_path(sub_m, labels, positive=True, max_iter=100, alphas=[alpha])[1]
-            coefs = coefs[:, -1]
-            coef_list.append(coefs)
-        coefs = numpy.concatenate(coef_list)
-        coefs = coefs/n_groups
+
+        if args.n_threads < 1:
+            args.n_threads = cpu_count()
+        n_threads = min(n_groups, args.n_threads)
+        if n_threads == 1:
+            coef_list = []
+            for i in xrange(n_groups):
+                begin = group_splits[i]
+                end = group_splits[i+1]
+                sub_m = m[:, begin:end]
+                coefs = sklearn.linear_model.lasso_path(sub_m, labels, positive=True, max_iter=100, alphas=[alpha])[1]
+                coefs = coefs[:, -1]
+                coef_list.append(coefs)
+            coefs = numpy.concatenate(coef_list)
+            coefs = coefs/n_groups
+        else:
+            in_qu = Queue()
+            out_qu = Queue()
+            procs = [Process(target=lars_worker, args=(in_qu, out_qu, m, labels, alpha)) for _ in xrange(n_threads)]
+            for i in xrange(n_groups):
+                begin = group_splits[i]
+                end = group_splits[i+1]
+                in_qu.put((i, begin, end))
+            for p in procs:
+                in_qu.put(None)
+                p.start()
+            coef_list = [None] * n_groups
+            for i in xrange(n_groups):
+                k, coefs = out_qu.get()
+                coef_list[k] = coefs
+            for p in procs:
+                p.join()
+            coefs = numpy.concatenate(coef_list)
+            coefs = coefs/n_groups
+
         # Use an additional l2 svm to refine the weights.
         nnz = coefs.nonzero()[0]
         m_sub = m[:, nnz]
